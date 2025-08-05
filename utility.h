013d34664d67d963539eb2fbd4ec644a2cfa9bd4
AngryFender <teabiscuitperson@gmail.com>
2025-08-05 23:31:50 +0100

    Added Serializable isolation transaction

--- utility.h
+++ utility.h
@@ -18,5 +18,32 @@ std::string currentTime()
     return ss.str();
 }
 
+void userInstrumentRequest(std::atomic_int& instrument_id,
+                           const int target_instrument_id,
+                           std::vector<pqxx::connection>& connection_collection,
+                           const int thread_no,
+                           const int user_id){
+
+    while(instrument_id.load() < target_instrument_id)
+    {
+        const int instrument_value = ++instrument_id;
+        while(1){
+            try {
+                pqxx::work txn(connection_collection[thread_no]);
+                txn.exec("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE");
+                pqxx::result result = txn.exec_prepared("try_insert", instrument_value, 1, 1, 1, user_id);
+                if(result.empty() || result[0][0].as<int>() == 0) {
+                    throw pqxx::sql_error();
+                }
+                txn.commit();
+                break;
+            } catch (pqxx::sql_error &e) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }
+        }
+    }
+
+
+}
 
 #endif //UTILITY_H
